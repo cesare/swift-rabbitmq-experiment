be4fff11ddc6e4c6aea546096e492e8bf625843c
SAWADA Tadashi <cesare@mayverse.jp>
2017-01-22 16:44:50 +0900

    Initial Commit

---
+++ RabbitExp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RabbitExp
+//
+//  Created by cesare on 2017/01/22.
+//  Copyright © 2017年 cesare. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
